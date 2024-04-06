7f83b274e50c858fdec4b1634b0210b8a1e10757
Nathan Reungoat <reungoat.nathan@gmail.com>
2024-04-06 21:27:12 +0200

    memo.txt added and start project fr

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <Arduino.h>
 #include "Motor.h"
 #include "HSensor.h"
+#include <time.h>
 
+long timeToWait = 0;
 
 void setup()
 {
   Serial.begin(9600);
-  Motor motor = New Motor(2);
-  HSensor sensor = New HSensor(3);
-  Serial.println(sensor.getHumidity()); 
+  Motor motor(2);
+  HSensor sensor(3);
+
+  // Récupération de l'heure actuelle
+  time_t currentTime;
+  time(&currentTime);
+  Serial.println(ctime(&currentTime));
+}
+
+int CalculateTimeToWait()
+{
 }
 
 void loop()
 {
-  
 }
